1383e2487e9c1511f6e312d7b78e532620aa76d1
ytsbside <shibamoto.yuta1229@gmail.com>
2020-12-13 15:18:29 +0000

    認証まわり実装

--- db/schema.rb
+++ db/schema.rb
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 2020_12_13_114007) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.string "name"
+    t.string "introduction"
+    t.string "profile_image_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
