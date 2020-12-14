78b4451787769b1feba986e8a0987e63676b07fc
ytsbside <shibamoto.yuta1229@gmail.com>
2020-12-14 22:31:43 +0000

    プロフィール編集機能実装

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '0e6f77bf5f8e920ef02111a8e2f46606c33fc3af4503426fe4ba9e04ac24d24170834477651f2246f460651bdf0ea0f16f1abcb01bac797b319eafcfb1014f20'
